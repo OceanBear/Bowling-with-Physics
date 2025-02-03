a918e40f83a30f5419a9c61cae6ec754cf2d7d52
Ocean_Bear <oceanbearx@gmail.com>
2025-02-03 15:27:13 -0800

    Added Cinemachine Camera and Gutters

---
+++ Assets/Gutter.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        Debug.Log("Ball entered the gutter!");
+        //first get the rigidbody of the ball
+        //store it in a local variable ballRigidBody
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+        if (ballRigidBody == null) return;
+        //store the velocity magnitude before resetting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+
+        //reset the linear and angular velocity
+        //cuz the ball is rotating on the ground when moving
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+
+        //add force in the forward direction of the gutter
+        //use the cached velocity magnitude to keep it a little realistic
+        ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
+    }
+}
