7c18d3cb701b8393c0415877df669b9f1a40ed6f
Ocean_Bear <oceanbearx@gmail.com>
2025-02-03 12:42:28 -0800

    Added Pins and the Ball works well

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,10 +5,14 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+
+    //name booleans like a question
+    private bool isBallLaunched;
     private Rigidbody ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isBallLaunched = false;
         //Grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
 
@@ -26,7 +30,9 @@ public class BallController : MonoBehaviour
     
     private void LaunchBall()
     {
+        if (isBallLaunched == true) return;
         //ForceMode.Impulse applies an instant force change
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse );
     }
 }
